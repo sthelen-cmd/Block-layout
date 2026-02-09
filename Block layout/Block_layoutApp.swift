34c3975f8485cbba73b1ce4c25f68cf73abe55c2
Samuel Thelen <sthelen@lcs.on.ca>
2026-02-09 14:24:35 -0500

    Initial Commit

---
+++ Block layout/Block_layoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  Block_layoutApp.swift
+//  Block layout
+//
+//  Created by Samuel Thelen on 09.02.26.
+//
+
+import SwiftUI
+
+@main
+struct Block_layoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Block layout/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Block layout
+//
+//  Created by Samuel Thelen on 09.02.26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
